470fa0d0ef826e82d17d28a9219e44410abbde49
DriedSponge <jordan@driedsponge.net>
2023-03-08 23:12:55 -0800

    I have no idea

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <svelte:head>
 	<title>A cool website that is yet to have a title...</title>
 </svelte:head>
-<h1 class="text-center text-2xl font-bold">
-	Hello World. Hopefully I will be given the time to do a demo.
-</h1>
+<h1 class="text-center text-2xl font-bold">Hello World. I need ideas of what to put here!</h1>
